cf233fe1ffb0a1638ad996e4cf5218bfcb8ea0d9
Aleksis Brezas <abresas@kamibu.com>
2011-07-28 11:37:52 +0000

    jshint on build

--- build/build.sh
+++ build/build.sh
@@ -1,16 +1,11 @@
-src_dir=$1
-if [ $# -ne 1 ]; then
-    echo "Assuming ../ as source directory"
-    src_dir=../
-fi
-if [ ! -d $src_dir ]; then
-    echo "Please give a valid directory as source directory"
-    exit 1;
-fi
+src_dir=`dirname $0`
+src_dir="$src_dir/../src"
+
 if ! jshint $src_dir; then
     echo ""
     echo "Please fix these errors before building"
     exit 1;
 fi
+
 echo "Building..."
 node build.js
