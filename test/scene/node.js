9656df0905903cf1016b20d938eaa10c0d4923d6
Aleksis Brezas <abresas@kamibu.com>
2011-08-01 09:49:18 +0000

    node testing

--- test/scene/node.js
+++ test/scene/node.js
@@ -1,7 +1,7 @@
 testcase = new Testcase();
 
-// simple checks on getters
-// (assume setters are correct)
+// test that after appendChild and removeChild 
+// parent-child relations are correct
 
 testcase.appendChild = function() {
     var root = new Node();
@@ -31,42 +31,102 @@ testcase.removeChild = function() {
     root.removeChild( n1 );
     this.assertEquals( root.children.length, 1, 'children length after removeChild' );
     this.assertEquals( root.children[ 0 ], n2, 'first child after removeChild' );
-    this.assertEquals( n1.parent, null, 'child parent null after removing from father' );
+    this.assertEquals( n1.parent, Node.Origin, 'child parent null after removing from father' );
 
     root.removeChild( n2 );
     this.assertEquals( root.children.length, 0, 'children length after all children removed' );
 };
 
+// test that getters are correct on scenes
+
 testcase.getAbsolutePosition = function() {
-    var node = new Node();
-    this.assertArrayEquals( node.getAbsolutePosition(), [ 0, 0, 0 ], 'getAbsolutePosition on new node' );
+    var root = new Node();
+    this.assertArrayEquals( root.getAbsolutePosition(), [ 0, 0, 0 ], 'getAbsolutePosition on new node' );
 
-    node.setAbsolutePosition( [ 10, 0, 5 ] ); 
-    node.assertArrayEquals( node.getAbsolutePosition(), [ 10, 0, 5 ], 'getAbsolutePosition after setAbsolutePosition' );
+    root.setPosition( [ 10, -2, 3 ] );
+    this.assertArrayEquals( root.getAbsolutePosition(), [ 10, -2, 3 ], 'getAbsolutePosition after setPosition' );
 
-    node.setAbsolutePosition( [ 0, 0, 0 ] );
-    var m = new Vector3();
-    node.getAbsolutePosition( m );
-    node.assertArrayEquals( m, [ 0, 0, 0 ], 'getAbsoltePosition after setting back to origin' );
+    var n1 = new Node();
+    root.appendChild( n1 );
+    this.assertArrayEquals( n1.getAbsolutePosition(), [ 10, -2, 3 ], 'getAbsolutePosition on child' );
+
+    n1.setPosition( [ -9, 1, -2 ]  );
+    this.assertArrayEquals( n1.getAbsolutePosition(), [ 1, -1, 1 ], 'getAbsolutePosition after setPosition on child' );
+
+    var n2 = new Node();
+    n2.getAbsolutePosition();  // test invalidation bug
+    n1.appendChild( n2 );
+    this.assertArrayEquals( n2.getAbsolutePosition(), [ 1, -1, 1 ], 'getAbsolutePosition on child after invalidating with getAbsolutePosition' );
+
+    root.removeChild( n1 );
+    this.assertArrayEquals( n1.getAbsolutePosition(), [ -9, 1, -2 ], 'getAbsolutePosition of child after removing grandfather' );
 };
 
 testcase.getAbsoluteOrientation = function() {
+    var root = new Node();
+    this.assertArrayEquals( root.getAbsoluteOrientation(), [ 0, 0, 0, 1 ], 'getAbsoluteOrientation on new node' );
+
+    root.setOrientation( [ 0.5, 0.5, 0.5, 0.5 ] );
+    this.assertArrayEquals( root.getAbsoluteOrientation(), [ 0.5, 0.5, 0.5, 0.5 ], 'getAbsoluteOrientation after setOrientation' );
+
+    var n1 = new Node();
+    root.appendChild( n1 );
+    this.assertArrayEquals( n1.getAbsoluteOrientation(), [ 0.5, 0.5, 0.5, 0.5 ], 'getAbsoluteOrientation on child' );
+
+    n1.setOrientation( [ 0.5, 0.5, 0.5, 0.5 ]  );
+    this.assertArrayEquals( n1.getAbsoluteOrientation(), [ 0.5, 0.5, 0.5, -0.5 ], 'getAbsoluteOrientation after setOrientation on child' );
+
+    var n2 = new Node();
+    n2.getAbsoluteOrientation();  // test invalidation bug
+    n1.appendChild( n2 );
+    this.assertArrayEquals( n2.getAbsoluteOrientation(), [ 0.5, 0.5, 0.5, -0.5 ], 'getAbsoluteOrientation on child after invalidating with getAbsoluteOrientation' );
+
+    root.removeChild( n1 );
+    this.assertArrayEquals( n1.getAbsoluteOrientation(), [ 0.5, 0.5, 0.5, 0.5 ], 'getAbsoluteOrientation of child after removing grandfather' );
 };
 
 testcase.getAbsoluteScale = function() {
+    var root = new Node();
+    this.assertEquals( root.getAbsoluteScale(), 1, 'getAbsoluteScale on new node' );
+
+    root.setScale( 2 );
+    this.assertEquals( root.getAbsoluteScale(), 2, 'getAbsoluteScale after setScale' );
+
+    var n1 = new Node();
+    root.appendChild( n1 );
+    this.assertEquals( n1.getAbsoluteScale(), 2, 'getAbsoluteScale on child' );
+
+    n1.setScale( 0.1 );
+    this.assertEquals( n1.getAbsoluteScale(), 0.2, 'getAbsoluteScale on child after setScale' );
 };
 
-// now check for the changes of setters on getMatrix
-// (assume getMatrix is correct)
 
+// test that setters work on scenes
 testcase.setAbsolutePosition = function() {
+    var node = new Node();
+
+    node.setAbsolutePosition( [ 10, 0, 5 ] ); 
+    this.assertArrayEquals( node.getAbsolutePosition(), [ 10, 0, 5 ], 'basic setAbsolutePosition' );
+
+    node.setAbsolutePosition( [ 0, 0, 0 ] );
+    this.assertArrayEquals( node.getAbsolutePosition(), [ 0, 0, 0 ], 'setAbsolutePosition back to origin' );
 };
 
 testcase.setAbsoluteOrientation = function() {
+    var node = new Node();
+
+    node.setAbsoluteOrientation( [ 0.5, 0.5, 0.5, 0.5 ] );
+    this.assertArrayEquals( node.getAbsoluteOrientation(), [ 0.5, 0.5, 0.5, 0.5 ], 'basic setAbsoluteOrientation' );
 };
 
 testcase.setAbsoluteScale = function() {
+    var node = new Node();
+
+    node.setAbsoluteScale( 2 );
+    this.assertEquals( node.getAbsoluteScale(), 2, 'basic setAbsoluteScale' );
 };
+
+// finally test getAbsoluteMatrix after various calls
 
 testcase.getAbsoluteMatrix = function() {
 };
