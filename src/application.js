290a3fa231271c8edca5d76221289002c4748ba5
petrosagg <petros@kamibu.com>
2011-08-02 03:37:49 +0300

    Application render loop with requestAnimationFrame

--- src/application.js
+++ src/application.js
@@ -1,17 +1,18 @@
 function Application() {
     this.renderManager = new RenderManager();
     this.scene = new Scene();
-    this.camera = new Camera();
+    this.camera = new Camera().setPosition( [ 0, 0, 10 ] );
 
     this.scene.root.appendChild( this.camera );
-    //this.camera.move( 0, 0, 10 );
 }
 
 Application.prototype = {
     start: function() {
-        setTimeout( function() {
-            this.renderManager.renderScene( this.scene, this.camera );
-        }.bind( this ), 3000 );
+        var self = this;
+        (function renderLoop() {
+            self.renderManager.renderScene( self.scene, self.camera );
+            window.requestAnimationFrame( renderLoop );
+        } )();
         return this;
     }
 };
