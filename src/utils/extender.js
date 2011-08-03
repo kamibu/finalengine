54e44affa5f778e10ce7c2df9082c87de5e47270
petrosagg <petros@kamibu.com>
2011-08-03 15:22:18 +0300

    Changed extender so the Object.isPrototypeOf method works correctly on inherited objects

--- src/utils/extender.js
+++ src/utils/extender.js
@@ -5,13 +5,37 @@
 */
 Object.defineProperty( Function.prototype, "extend", {
     value: function( parent ) {
-        for ( var method in parent.prototype ) {
+        // Use an empty function because firefox does not support setting the __proto__ value of any object
+        function Empty() {}
+        Empty.prototype = parent.prototype;
+
+        //Save the child prototype because it will be overwritten by the Empty function's instance
+        var childPrototype = this.prototype;
+
+        this.prototype = new Empty();
+
+        //Copy the values of the childPrototype back into this.prototype
+        for ( var method in childPrototype ) {
+            this.prototype[ method ] = childPrototype[ method ];
+        }
+
+        //Continue with the overriding handling
+        for ( method in parent.prototype ) {
+            //Every prototype has the property constructor. No need to override.
             if ( method == 'constructor' ) {
                 continue;
             }
+            
+            /* If a parent method is overrided provide a way to call it by setting 
+             * the ParentClass_overridedMethod method on child's prototype
+             */
             if ( this.prototype[ method ] ) {
                 this.prototype[ parent.name + '_' + method ] = parent.prototype[ method ];
             }
+            /* TODO Test if setting non-overrided parent methods on the child prototype is nessesary.
+             * Only reason is for performance in method name resolution, otherwise they are already 
+             * available because child.prototype.__proto__ == parent.prototype
+             */
             else {
                 this.prototype[ method ] = parent.prototype[ method ];
             }
