4c8df7811ac2fcc07e333626a044a0f114b195ca
petrosagg <petros@kamibu.com>
2011-08-12 17:51:26 +0300

    Build system improvement

--- build/build.js
+++ build/build.js
@@ -1,66 +1,47 @@
-var debug = true;
-var root = '../src/';
+//JSHint Options
+var options = {
+    curly: true,
+    immed: true,
+    newcap: true,
+    undef: true,
+    browser: true,
+    jquery: true
+}
 
-var files = [
-    'debug/assert.js',
-    'utils/uuid.js',
-    'utils/extender.js',
-
-    'network/request.js',
-
-    'events/eventemitter.js',
-    'events/eventwaiter.js',
-
-    'math/number.js',
-    'math/matrix4.js',
-    'math/vector3.js',
-    'math/quaternion.js',
-    'math/transform.js',
-
-    'utils/tempvars.js',
-    'utils/bounds.js',
-    'utils/RequestAnimationFrame.js',
-
-    'renderer/renderer.js',
-    'renderer/buffer.js',
-    'renderer/vertexbuffer.js',
-    'renderer/texture.js',
-    'renderer/mesh.js',
-    'renderer/shader.js',
-    
-    'bounding/boundingvolume.js',
-    'bounding/boundingsphere.js',
-    'bounding/boundingbox.js',    
-
-    'scene/scene.js',
-    'scene/node.js',
-    'scene/camera.js',
-    'scene/drawable.js',
-
-    'material/material.js',
-
-    'geometry/cube.js',
-
-    'texturemanager.js',
-    'rendermanager.js',
-    'application.js',
-
-    'exporter/exporter.js',
-    'exporter/importer.js',
-
-    'test/testcase.js',
-    'test/browser/testview.js'
-];
-
-
-var debugRegExp = /\/\*DEBUG\*\/(.|\n|\r)*?\/\*DEBUG_END\*\//g;
+var JSHINT = require( './jshint' ).JSHINT;
 var fs = require( 'fs' );
+
+var files = fs.readFileSync( __dirname + '/files.list', 'utf8' ).split( '\n' ).filter( function( file ) {
+    return file;
+} ).map( function( file ) {
+    return file.trim();
+} );
+
+var root = '../src/';
+var debugRegExp = /\/\*DEBUG\*\/(.|\n|\r)*?\/\*DEBUG_END\*\//g;
+
+var lintFree = true;
 var res = '';
-var file = '';
-for( var i = 0; i < files.length; ++i ) {
-    res += fs.readFileSync( root + files[ i ], 'utf8' );
-}
-if( !debug ) {
+files.forEach( function( file ) {
+    var source = fs.readFileSync( __dirname + '/' + root + file, 'utf8' );
+    if ( JSHINT( source, options ) ) {
+        res += source;
+    }
+    else {
+        JSHINT.errors.forEach( function( error ) {
+            console.log( file + ': line ' + error.line + ', col ' + error.character + ', ' + error.reason );
+        } );
+        lintFree = false;
+    }
+} );
+
+if ( lintFree ) {
+    console.log( 'Lint Free!' );
+    console.log( 'Building..' );
+    fs.writeFileSync( __dirname + '/final-engine-debug.js', res );
     res = res.replace( debugRegExp, '' );
+    fs.writeFileSync( __dirname + '/final-engine.js', res );
 }
-fs.writeFileSync( 'final-engine.js', res );
+else {
+    console.log( 'Please fix these errors before building' );
+}
--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,4 @@
+#!/bin/bash
 cur_dir=`dirname $0`
 cd $cur_dir
-
-if ! jshint ../src; then
-    echo ""
-    echo "Please fix these errors before building"
-    exit 1;
-fi
-
-echo -n "Building..."
 node build.js
-echo "OK"
