254acd7b70d7aac7b637c8de520cdef78ccaa791
petrosagg <petros@kamibu.com>
2011-08-15 15:24:23 +0300

    Simple working rendermanager

--- src/rendermanager.js
+++ src/rendermanager.js
@@ -1,10 +1,9 @@
 // extern
-var Matrix4, Renderer;
+var Matrix4, Renderer, Drawable;
 
 function RenderManager() {
     this.renderer = new Renderer();
     this.forcedMaterial = null;
-    this.camera = null;
 
     this.globalUniformCache = {
         Time: Date.now(),
@@ -20,53 +19,25 @@ function RenderManager() {
 
 RenderManager.prototype = {
     constructor: RenderManager,
-    getParameter: function( name ) {
-        var g = this.globalUniformCache;
-        switch ( name ) {
-            case 'Time':
-                if ( !g.Time ) {
-                    g.Time = Date.now();
-                }
-                return g.Time;
-            case 'ProjectionMatrix':
-                return g.ProjectionMatrix;
-            case 'ViewMatrix':
-                return g.ViewMatrix;
-            case 'WorldMatrix':
-                return g.WorldMatrix;
-            case 'ViewProjectionMatrix':
-                if ( !g.ViewProjectionMatrix ) {
-                    g.ViewProjectionMatrix.set( this.camera.projectionMatrix ).multiply( g.ViewMatrix );
-                }
-                return g.ViewProjectionMatrix;
-            case 'WorldViewMatrix':
-                if ( !g.WorldViewMatrix ) {
-                    g.WorldViewMatrix.set( g.ViewMatrix ).multiply( g.WorldMatrix );
-                }
-                return g.WorldViewMatrix;
-            case 'WorldViewProjectionMatrix':
-                if ( !g.WorldViewProjectionMatrix ) {
-                    g.WorldViewProjectionMatrix.set( g.ProjectionMatrix ).multiply( g.ViewMatrix ).multiply( g.WorldMatrix );
-                }
-                return g.WorldViewProjectionmatrix;
-        }
-    },
     renderScene: function( scene, camera ) {
-        this.camera = camera;
         this.renderer.clear();
         var g = this.globalUniformCache;
-        g.ProjectionMatrix.set( camera.projectionMatrix );
+        camera.projectionMatrix.setTo( g.ProjectionMatrix );
         camera.getInverseMatrix( g.ViewMatrix );
+        g.ViewProjectionMatrix.set( g.ProjectionMatrix ).multiply( g.ViewMatrix );
 
         var bucket = [];
         function fillDrawBucket( node ) {
-            if ( node.mesh ) {
+            if ( Drawable.prototype.isPrototypeOf( node ) ) {
                 bucket.push( node );
             }
-            var l = node.children.length;
+            var children = node.children;
+            var l = children.length;
             while ( l-- ) {
-                fillDrawBucket( node.children[ l ] );
+                fillDrawBucket( children[ l ] );
             }
+            //I have a dream. Where array.forEach is faster than looping
+            //node.children.forEach( fillDrawBucket );
         }
         fillDrawBucket( scene.root );
 
@@ -74,16 +45,18 @@ RenderManager.prototype = {
         bucket.sort( function( a, b ) {
             return a.material.uid - b.material.uid;
         } );
-
+        var currentMaterial = -1;
         var l = bucket.length;
         while ( l-- ) {
             var currentDrawable = bucket[ l ];
             currentDrawable.onBeforeRender( camera );
             currentDrawable.getMatrix( g.WorldMatrix );
+            g.WorldViewMatrix.set( g.ViewMatrix ).multiply( g.WorldMatrix );
+            g.WorldViewProjectionMatrix.set( g.ViewProjectionMatrix ).multiply( g.WorldMatrix );
 
             var material = currentDrawable.material;
             for ( var engineParameter in material.engineParameters ) {
-                material.set( engineParameter, this.getParameter( engineParameter ) );
+                material.setParameter( engineParameter, g[ engineParameter ] );
             }
 
             this.renderer.useShader( material.getShader() );
