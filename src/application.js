7ebb9a2a888caba9794da9fabaa797cdf635be15
petrosagg <petros@kamibu.com>
2011-08-15 15:50:34 +0300

    Simpler main Application class

--- src/application.js
+++ src/application.js
@@ -1,33 +1,43 @@
 // extern
-var Camera, RenderManager, Scene;
+var Camera, RenderManager, Scene, Importer, Exporter;
 
 function Application() {
     this.renderManager = new RenderManager();
     this.scene = new Scene();
     this.camera = new Camera().setPosition( [ 0, 0, 10 ] );
 
+    this.importer = new Importer( 'resources' );
+    this.exporter = new Exporter( 'resources' );
+
     this.scene.root.appendChild( this.camera );
+
+    this.setupCanvas( this.renderManager.renderer.canvas );
+
+    this.handleResize();
+    window.addEventListener( 'resize', this.handleResize.bind( this ), false );
+    
+    var self = this;
+    ( function renderLoop() {
+        self.renderManager.renderScene( self.scene, self.camera );
+        window.requestAnimationFrame( renderLoop );
+    }() );
 }
 
 Application.prototype = {
-    start: function() {
-        var self = this;
+    constructor: Application,
+    setupCanvas: function( canvas ) {
+        document.body.appendChild( canvas );
+        return this;
+    },
+    handleResize: function() {
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        this.renderManager.renderer.setSize( w, h );
 
-        document.body.appendChild( this.renderManager.renderer.canvas );
-
-        function snapToWindow() {
-            self.renderManager.renderer.setSize( window.innerWidth, window.innerHeight );
-        }
-        snapToWindow();
-        window.addEventListener( 'resize', snapToWindow, false );
-
-        function renderLoop() {
-            self.renderManager.renderScene( self.scene, self.camera );
-            window.requestAnimationFrame( renderLoop );
-        }
-
-        renderLoop();
-
+        var camera = this.camera;
+        camera.width = w;
+        camera.height = h;
+        camera.setPerspective();
         return this;
     }
 };
