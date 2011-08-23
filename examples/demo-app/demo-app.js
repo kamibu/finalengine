791bd6385c142fe63b4890ab808d8fde245c0cbc
petrosagg <petros@kamibu.com>
2011-08-23 20:28:49 +0300

    Stuff

--- examples/demo-app/demo-app.js
+++ examples/demo-app/demo-app.js
@@ -1,28 +1,24 @@
+var foo = function(){};
 function DemoApp() {
     Application.call( this );
     var self = this;
 
-    this.importer.load( 'woman_Low', function( woman ) {
-        self.scene.add( woman );
-        setInterval( function() {
-            woman.rotate( [ 0, 1, 0 ], 0.01 );
-        }, 17 );
+//    this.importer.load( 'streets_Asphalt_drawable', function( woman ) {
+//        self.scene.add( woman );
+//        window.node = woman;
+//        setInterval( function() {
+//            woman.rotate( [ 0, 1, 0 ], 0.01 );
+//        }, 17 );
+//    } );
+
+//    console.log( 'Loading..' );
+    new OBJLoader().loadOBJ( 'futuristic2/futuristic_0.obj', function( node ) {
+        console.log( 'Loaded' );
+        window.node = node;
+        node.name = 'fp_city';
     } );
+    this.camera.setPosition( [ 0, 2, 0 ] );
 
-    /*
-     * This block of code loads an OBJ and renders it.
-     *
-    new OBJLoader().loadOBJ( 'woman/Woman_Low.obj', function( node ) {
-        self.scene.add( node );
-        console.log( 'Done' );
-
-        //To save the loaded mesh first set a name to node, for example node.name = 'woman high detail'
-        //and then do self.exporter.save( node ); After you do this the model will be saved in the folder
-        //./resources. Use the code above to load the JSON that you just created.
-    } );
-    */
-
-    this.camera.setPosition( [ 0, 1, 3 ] );
 }
 DemoApp.extend( Application );
 
--- examples/demo-app/resources/beautify.js
+++
@@ -1,2 +0,0 @@
-var uglyJSON = require( 'fs' ).readFileSync( process.argv[ 2 ], 'utf-8' );
-console.log( JSON.stringify( JSON.parse( uglyJSON ), null, 4 ) );
