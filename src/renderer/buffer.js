8cc964e251adf9575af5f066cd8c1e870e034721
Aleksis Brezas <abresas@kamibu.com>
2011-08-24 22:56:29 +0300

    buffer export utf optimization

--- src/renderer/buffer.js
+++ src/renderer/buffer.js
@@ -61,20 +61,97 @@ Buffer.prototype = {
         this.data = data;
         this.length = data.length;
     },
+    FloatToUtf8: function( a ) {
+        var u = new Uint16Array( a.buffer ),
+            s = "",
+            l = u.length;
+        for ( var i = 0; i < l; ++i ) {
+            s += String.fromCharCode( u[ i ] );
+        }
+        return s;
+    },
+    Uint16ToUtf8: function( u ) {
+        var s = "",
+            l = u.length;
+        for ( var i = 0; i < l; ++i ) {
+            var a = u[ i ];
+            if ( a < 32 ) {
+                s += String.fromCharCode( 65535 - a );
+            }
+            else if ( a == 34 ) {
+                // 65535 - 32
+                s += String.fromCharCode( 65503 );
+            }
+            else if ( a == 92 ) {
+                // 65535 - 33
+                s += String.fromCharCode( 65502 );
+            }
+            else if ( a < 65502 ) {
+                s += String.fromCharCode( a );
+            }
+            else if ( a == 65502 ) {
+                s += String.fromCharCode( 92 );
+            }
+            else if ( a == 65503 ) {
+                s += String.fromCharCode( 34 );
+            }
+            else {
+                s += String.fromCharCode( 65535 - a );
+            }
+        }
+        return s;
+    },
+    Utf8ToFloat: function( s ) {
+        var u = new Uint16Array( s.length ),
+            l = s.length;
+        for ( var i = 0; i < l; ++i ) {
+            u[ i ] = s.charCodeAt( i );
+        }
+        return new Float32Array( u.buffer );
+    },
+    Utf8ToUint16: function( s ) {
+        var u = new Uint16Array( s.length ),
+            l = s.length;
+        for ( var i = 0; i < l; ++i ) {
+            var a = s.charCodeAt( i );
+            if ( a > 65503 ) {
+                u[ i ] = 65535 - a;
+            }
+            else if ( a == 65503 ) {
+                u[ i ] = 34;
+            }
+            else if ( a == 65502 ) {
+                u[ i ] = 92;
+            }
+            else if ( a == 92 ) {
+                u[ i ] = 65502;
+            }
+            else if ( a == 34 ) {
+                u[ i ] = 65503;
+            }
+            else if ( a < 32 ) {
+                u[ i ] = 65535 - a;
+            }
+            else {
+                u[ i ] = a;
+            }
+        }
+        return u;
+    },
     getExportData: function( exporter ) {
         var ret = {
             name: this.name,
             usage: this.usage,
-            type: this.type,
-            data: this.data.toArray()
+            type: this.type
         };
+        ret.data = ( this.type == Buffer.DATA_BUFFER ? this.FloatToUtf8( this.data ) : this.Uint16ToUtf8( this.data ) );
         return ret;
     },
     setImportData: function( importer, data ) {
         this.name = data.name;
         this.usage = data.usage;
         this.type = data.type;
-        this.setData( data.data );
+        this.setData( this.type == Buffer.DATA_BUFFER ? this.Utf8ToFloat( data.data ) : this.Utf8ToUint16( data.data ) );
         return this;
     }
 };
