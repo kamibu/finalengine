63a285a93642727fcf856bf3dfb922ed85583500
petrosagg <petros@kamibu.com>
2011-09-01 14:32:18 +0300

    Animable

--- src/animation/animable.js
+++ src/animation/animable.js
@@ -4,15 +4,41 @@ var Drawable, Node;
 function Animable(){
     Drawable.call( this );
     this.skeleton = null;
+    this.animations = null;
+
+    this.activeAnimation = null;
+    this.started = 0;
 }
 
 Animable.prototype = {
     constructor: Animable,
+    playAnimation: function( name ) {
+        this.activeAnimation = this.animations[ name ];
+        this.started = Date.now();
+    },
     onBeforeRender: function( camera ) {
-        this.skeleton.updateJointUniforms();
-        //Βαγγέλης Νικολουδάκης
-        this.material.setParameter( 'JointPositionsAndScales', this.skeleton.jointPositionsAndScales );
-        this.material.setParameter( 'JointOrientations', this.skeleton.jointOrientations );
+        var skeleton = this.skeleton;
+        var animation = this.activeAnimation;
+        if ( animation ) {
+            var keyframe = ( ( ( Date.now() - started ) / animation.duration ) * animation.keyframes ) % animation.keyframes;
+            keyframe |= 0;
+
+            var l = animation.matrices.length;
+            TempVars.lock();
+            var q = TempVars.getQuaternion();
+            while ( l-- ) {
+                var m = animation.matrices[ l ][ keyframe ];
+
+                q.fromMatrix3( m );
+                d.skeleton.jointSlots[ l ].setOrientation( q );
+                d.skeleton.jointSlots[ l ].setScale( Math.sqrt( m[ 0 ] * m[ 0 ] + m[ 1 ] * m[ 1 ] + m[ 2 ] * m[ 2 ] ) );
+            }
+            TempVars.release();
+        }
+
+        skeleton.updateJointUniforms();
+        this.material.setParameter( 'JointPositionsAndScales', skeleton.jointPositionsAndScales );
+        this.material.setParameter( 'JointOrientations', skeleton.jointOrientations );
     },
     getExportData: function( exporter ) {
         var ret = {};
