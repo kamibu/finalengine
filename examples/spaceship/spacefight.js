058273ed9f4ced7f0d76759a6b951d7fd9fd8ace
Aleksis Brezas <abresas@kamibu.com>
2011-10-05 16:55:37 +0300

    spaceship demo fixes

--- examples/spaceship/spacefight.js
+++ examples/spaceship/spacefight.js
@@ -2,8 +2,35 @@ var app = new Application();
 app.setTitle( 'SpaceFight 3D' );
 app.camera.setPosition( new Vector3( [ 0, 5, 20 ] ) );
 
-var spaceship = new Spaceship( 'spaceship.obj', app.importer, app.input );
+var spaceship = new Spaceship();
 app.scene.appendChild( spaceship );
+
+app.input.onKey( 'RIGHT_ARROW',{
+    callback: function() {
+        spaceship.targetRoll = -45;
+    },
+    endCallback: function() {
+        spaceship.targetRoll = 0; 
+    }
+} );
+
+app.input.onKey( 'LEFT_ARROW', {
+    callback: function() {
+        spaceship.targetRoll = 45;
+    },
+    endCallback: function() {
+        spaceship.targetRoll = 0;
+    }
+} );
+
+app.input.onKey( 'SPACE', function() {
+    spaceship.shoot();
+} );
+
+app.importer.load( 'spaceship.obj', function( model ) {
+    model.rotate( new Vector3( [ 1, 0, 0 ] ), -Math.PI / 2 ); // fix orientation
+    spaceship.appendChild( model );
+} );
 
 app.update = function( dt ) {
     spaceship.update( dt );
--- examples/spaceship/spaceship.js
+++ examples/spaceship/spaceship.js
@@ -1,55 +1,15 @@
-function Spaceship( modelfile, importer, input ) {
-    var self = this;
-
+function Spaceship( modelfile, importer ) {
     Node.call( this );
 
     this.bullets = [];
     this.velocity = 0.5;
 
-    importer.load( modelfile, function( model ) {
-        self.onModelLoad( model );
-    } );
-
-    this.zOrientation = this.getOrientation();
-    this.base = new Quaternion( [ 0, 0, 0, 1 ] );
-    this.left = new Quaternion( [ 0, 0, 0.707, 0.707 ] );
-    this.right = new Quaternion( [ 0, 0, -0.707, 0.707 ] );
-    this.targetOrientation = this.base;
+    this.targetRoll = 0;
     this.yAngle = 0;
-
-    input.onKey( 'RIGHT_ARROW', {
-        callback: function() {
-            self.targetOrientation = self.right;
-            self.yAngle -= 0.01;
-        },
-        endCallback: function() {
-            self.targetOrientation = self.base;
-        },
-        repeat: true
-    } );
-
-    input.onKey( 'LEFT_ARROW', {
-        callback: function() {
-            self.targetOrientation = self.left;
-            self.yAngle += 0.01;
-        },
-        endCallback: function() {
-            self.targetOrientation = self.base;
-        },
-        repeat: true
-    } );
-
-    input.onKey( 'SPACE', function() {
-        self.shoot();
-    } );
 }
 
 Spaceship.prototype = {
     constructor: Spaceship,
-    onModelLoad: function( model ) {
-        model.rotate( new Vector3( [ 1, 0, 0 ] ), -Math.PI / 2 ); // fix orientation
-        this.appendChild( model );
-    },
     shoot: function() {
         var leftBullet = new Bullet( this.yAngle );
         leftBullet.setPosition( new Vector3( [ -3, 0, 0 ] ) );
@@ -67,14 +27,12 @@ Spaceship.prototype = {
         if ( !dt ) {
             return this.Node_update.call( this );
         }
+    
+        this.yAngle += this.targetRoll / 4500;
+        
+        var targetOrientation = new Quaternion().setEuler( this.yAngle, 0, this.targetRoll );
+        this.setOrientation( this.getOrientation().slerp( targetOrientation, dt / 1000 ) );
 
-        var turnSpeed = this.targetOrientation == this.base ? 1 : 1;
-
-        //console.log( this.zOrientation, this.targetOrientation, turnSpeed, dt );
-        this.setOrientation( this.zOrientation.slerp( this.targetOrientation, turnSpeed * dt / 1000 ) );
-        //console.log( this.zOrientation, this.targetOrientation, turnSpeed, dt );
-        //console.log( '---' );
-        this.rotate( new Vector3( [ 0, 1, 0 ] ), this.yAngle );
         this.move( new Vector3( [ -this.velocity * Math.sin( this.yAngle ), 0, -this.velocity * Math.cos( this.yAngle ) ] ) );
 
         for ( var i = 0; i < this.bullets.length; ++i ) {
