a990d6b3a26d6c2654ece072120200ea98315dda
Aleksis Brezas <abresas@kamibu.com>
2011-10-25 19:59:21 +0300

    remove jsdoc documentation

--- docs/jsdoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -n "$1" ]; then
-    files=$1
-else
-    files="../src"
-fi
-
-java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -t=jsdoc-toolkit/templates/finalengine -d=doc-generated -r=4 $files
-#rm -rf doc-generated/symbols/src
